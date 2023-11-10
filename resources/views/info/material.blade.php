24edeef41d9e4d006eb278845c0cd5d4b470a074
System Administrator <root@MacBook-Pro-de-Garfias.local>
2023-11-10 13:31:13 -0600

    Integracion del Apartado de material de apoyo

---
+++ resources/views/info/material.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.administrador')
+
+@section('encabezado')
+    <!--Heading-->
+    <h1 class="h3 mb-0 text-gray-800">Material de Apoyo</h1>
+@endsection
+
+@section('content')
+    <div class="col-lg-12 mb-4">
+        <div class="card shadow mb-4">
+            <div class="card-header py-3" style="background-color: #681b2e;">
+                <h6 class="m-0 font-weight-bold text-light">Documentos descargables</h6>
+            </div>
+            <div class="card-body">
+                <center >
+                    <div style="width:50%;border-radius:15px">
+                        <table style="width: 100%;text-align:center" class="table striped">
+                            <thead style="background-color: gray;color:white">
+                                <tr>
+                                    <th>No.</th>
+                                    <th>Documento</th>                                    
+                                    <th>Descarga</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Manual de Usuario SIIBIEN V1.0</td>                                    
+                                    <td><button class="btn btn-primary">PDF</button></td>
+                                </tr>
+                                <tr>
+                                    <td>2</td>
+                                    <td>Herramienta para la Proyección de metas</td>
+                                    <td><button class="btn btn-success">Excel</button></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </center>
+            </div>
+        </div>
+    </div>
+@endsection
