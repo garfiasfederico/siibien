ad38273c22da050a3aae1888309e003e10bb19f4
System Administrator <root@MacBook-Pro-de-Garfias.local>
2023-11-15 16:35:46 -0600

    Integracion de Registro de asistencia para enlaces participantes

---
+++ resources/views/layouts/temporal.blade.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>SIIBien</title>
+
+    <link rel="icon" href="{{ asset('images/icon_.png') }}" type="image/ico" />
+    <link href="{{ asset('resources/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('resources/css/sb-admin-2.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('resources/vendor/sweetalert/css/sweetalert2.min.css') }}" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+    @yield('styles')
+    <style>
+        .ligas a:link {
+            color: black;
+            text-decoration: none;
+        }
+
+        .ligas a:hover {
+            font-size: 1em; 
+            text-decoration: underline red;           
+        }
+    </style>
+
+
+    <!-- Custom styles for this template-->
+</head>
+
+<body class="font-sans antialiased" style="background-image:url('{{ asset('resources/images/logo_bg.png') }}')"
+    id="page-top">
+    <div id="wrapper">
+        <div id="content-wrapper" class="d-flex flex-column">
+            <!-- Main Content -->
+            <div id="content">
+                <div style="background-color:black;color:white;text-align:center; height:50px;display:table;width:100%">
+                    <div style="display:table-cell; vertical-align:middle">
+                        "2023 AÑO DE LA INTERCULTURALIDAD"
+                    </div>
+                </div>
+                <div
+                    style="background-image: url(images/main/cintillo.svg);width:100%;height:50px;background-repeat:no-repeat;background-size: 100%">
+                </div>   
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                        @yield('encabezado')
+                    </div>
+                    <div style="background-image:url('{{ asset('resources/images/logo_bg.png') }}');background-size:50px;">
+                        @yield('content')
+                    </div>
+                </div>
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
+
+  
+        </div>
+        <!-- Page Content -->
+
+    </div>
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+
+
+   <!-- <script src="{{ asset('resources/vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('resources/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
+
+    
+    <script src="{{ asset('resources/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+
+    
+    <script src="{{ asset('resources/js/sb-admin-2.min.js') }}"></script>-->
+
+</body>
+
+
+</html>
---
+++ resources/views/temporal/registroasistencia.blade.php
@@ -0,0 +1,98 @@
+@extends('layouts.temporal')
+@section('content')
+    <center>
+        <div class="col-lg-6 mb-6">
+            <!-- Pendientes IE -->
+            <div class="card shadow mb-6">
+                <div class="card-header py-4" style="background-color: #681b2e;">
+                    <h6 class="m-0 font-weight-bold text-light">Seguimiento a Indicadores de Gobierno</h6>
+                </div>
+                <img style="width:200px;position:absolute; top:0px;" src="{{ asset('/images/logo_blanco.png') }}" />
+                <div class="card-body">
+                    <center style="overflow:auto">
+                        <table class="table table-light" style="width: 100%">
+                            <tbody>
+                                <tr style="text-align: center">
+                                    <td style="border: solid 1px rgb(175,119,130);"><img style="width:400px;"
+                                            src="{{ asset('/images/siibien_colores.png') }}"</td>
+                                    <td
+                                        style=" vertical-align:middle;background-color:rgb(175,119,130);color:white; text-align:center">
+                                        <h2>Registro de Asistencia a la Capacitación de Enlaces</h2>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <hr>
+                    </center>
+                    <form novalidate method="POST" action="{{ route('registraasistencia') }}">
+                        @csrf
+                        <div class="row text-left" style="color: black">
+                            <div class="col-lg-12 mb-12">
+                                <label for="nombre">Nombre Completo<span style="color: red">*</span></label>
+                                <input type="text" class="form-control py-4" id="nombre" name="nombre"
+                                    placeholder="Nombre del Enlace" value="{{old('nombre')}}" >
+                                @error('nombre')
+                                    <div class="alter alert-danger py-2 p-1">
+                                        Ingresar Nombre Completo
+                                    </div>
+                                @enderror
+
+                            </div>
+                            <div class="col-lg-12 mb-12 p-3">
+                                <label for="dependencia">Dependencia<span style="color: red">*</span></label>
+                                <select class="form-control py-2" id="dependencia" name="dependencia" required>
+                                    <option value="" selected>Seleccione...</option>
+                                    @foreach ($dependencias as $dependencia)
+                                        <option value="{{ $dependencia->idDependencia }}">
+                                            {{ $dependencia->dependenciaNombre . ' (' . $dependencia->dependenciaSiglas . ')' }}
+                                        </option>
+                                    @endforeach
+                                </select>
+                                @error('dependencia')
+                                    <div class="alter alert-danger py-2 p-1">
+                                        Seleccione Dependencia
+                                    </div>
+                                @enderror
+                            </div>
+                            <div class="col-lg-12 mb-12 p-3">
+                                <label for="cargo">Cargo<span style="color: red">*</span></label>
+                                <input type="text" class="form-control py-4" id="cargo" name="cargo" value="{{old('cargo')}}"
+                                    placeholder="Cargo que desempeña" required>
+                                @error('cargo')
+                                    <div class="alter alert-danger py-2 p-1">
+                                        Indicar el cargo que desempeña
+                                    </div>
+                                @enderror
+                            </div>
+                            <div class="col-lg-12 mb-12 p-3">
+                                <label for="email">Correo Electrónico<span style="color: red">*</span></label>
+                                <input type="text" class="form-control py-4" id="email" name="email" value="{{old('email')}}"
+                                    placeholder="ejemplo@ejemplo.com" required>
+                                @error('email')
+                                    <div class="alter alert-danger py-2 p-1">
+                                        Indicar correo electrónico válido
+                                    </div>
+                                @enderror
+                            </div>
+                            <div class="col-lg-12 mb-12 p-3">
+                                <label for="telefono">Teléfono de contacto<span style="color: red">*</span></label>
+                                <input type="text" class="form-control py-4" id="telefono" name="telefono" value="{{old('telefono')}}"
+                                    placeholder="" required>
+                                @error('telefono')
+                                    <div class="alter alert-danger py-2 p-1">
+                                        Indicar Teléfono de contacto
+                                    </div>
+                                @enderror
+                            </div>
+                            <div class="col-lg-12 mb-12 p-3 text-right">
+                                <a href="{{route('registro')}}"><button type="button" class="btn btn-secondary">Cancelar</button></a>
+                                <button type="submit" class="btn" style="background-color: #681b2e; color:white">
+                                    <h5>Registrar Asistencia</h5>
+                                </button>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </center>
+@endsection
