915ff9798d5b1f209675d16e2ac001d0c249fc22
System Administrator <root@MacBook-Pro-de-Garfias.local>
2023-11-24 13:50:24 -0600

    Discriminacion de indicadores desde el administrador

--- resources/views/super/indicadoredit.blade.php
+++ resources/views/super/indicadoredit.blade.php
@@ -955,7 +955,7 @@
 
         function validaVariables() {
             valid = true;
-            if ($(".variable").length < 2) {
+            if ($(".variable").length < 2 && indicador!=74 && indicador!=9  && indicador!=10 && indicador!=20 && indicador!=75 && indicador!=76 && indicador!=77 && indicador!=78 && indicador!=79 && indicador!=82 && indicador!=112) {
                 Swal.fire({
                     icon: 'warning',
                     title: 'Cantidad insufiente de variables registradas',
@@ -987,7 +987,7 @@
             valid = validaIndicador();
 
             if (valid) {
-                if ($(".variable").length < 2) {
+                if ($(".variable").length < 2 && indicador!=74 && indicador!=9  && indicador!=10 && indicador!=20 && indicador!=75 && indicador!=76 && indicador!=77 && indicador!=78 && indicador!=79 && indicador!=82 && indicador!=112) {
                     Swal.fire({
                         icon: 'warning',
                         title: 'Cantidad insufiente de variables registradas',
