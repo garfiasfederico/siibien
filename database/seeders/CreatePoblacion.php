4cf3337363d5a983abb68d0adbc4b67bd20ce821
Federico Sanchez <garfias.federico@gmail.com>
2024-07-08 09:52:08 -0600

    Adecuaciones al ITAR

--- database/seeders/CreatePoblacion.php
+++ database/seeders/CreatePoblacion.php
@@ -15,26 +15,26 @@ class CreatePoblacion extends Seeder
      */
     public function run()
     {
-        Poblacion::create(["descripcion"=>"Adolescente"]);
-        Poblacion::create(["descripcion"=>"Adulto(a)"]);
-        Poblacion::create(["descripcion"=>"Adulto(a) Mayor"]);
-        Poblacion::create(["descripcion"=>"Alumno(a)"]);
-        Poblacion::create(["descripcion"=>"Artesano(a)"]);
-        Poblacion::create(["descripcion"=>"Defraudado(a) por caja de ahorro"]);
-        Poblacion::create(["descripcion"=>"Docente"]);
-        Poblacion::create(["descripcion"=>"Elemento policial"]);
-        Poblacion::create(["descripcion"=>"Familia"]);
-        Poblacion::create(["descripcion"=>"Indígena y/o afromexicano"]);
-        Poblacion::create(["descripcion"=>"Joven"]);
-        Poblacion::create(["descripcion"=>"Madre soltera"]);
-        Poblacion::create(["descripcion"=>"Migrante"]);
-        Poblacion::create(["descripcion"=>"Mujer"]);
-        Poblacion::create(["descripcion"=>"Niño(a)"]);
-        Poblacion::create(["descripcion"=>"Otro(a)"]);
-        Poblacion::create(["descripcion"=>"Persona con discapacidad"]);
-        Poblacion::create(["descripcion"=>"Pescador(a)"]);
+        Poblacion::create(["descripcion"=>"Adolescentes"]);
+        Poblacion::create(["descripcion"=>"Adultos(as)"]);
+        Poblacion::create(["descripcion"=>"Adultos(as) Mayores"]);
+        Poblacion::create(["descripcion"=>"Alumnos(as)"]);
+        Poblacion::create(["descripcion"=>"Artesanos(as)"]);
+        Poblacion::create(["descripcion"=>"Defraudados(as) por caja de ahorro"]);
+        Poblacion::create(["descripcion"=>"Docentes"]);
+        Poblacion::create(["descripcion"=>"Elementos policiales"]);
+        Poblacion::create(["descripcion"=>"Familias"]);
+        Poblacion::create(["descripcion"=>"Indígenas y/o afromexicanos"]);
+        Poblacion::create(["descripcion"=>"Jóvenes"]);
+        Poblacion::create(["descripcion"=>"Madres solteras"]);
+        Poblacion::create(["descripcion"=>"Migrantes"]);
+        Poblacion::create(["descripcion"=>"Mujeres"]);
+        Poblacion::create(["descripcion"=>"Niños(as)"]);
+        Poblacion::create(["descripcion"=>"Otros(as)"]);
+        Poblacion::create(["descripcion"=>"Personas con discapacidad"]);
+        Poblacion::create(["descripcion"=>"Pescadores(as)"]);
         Poblacion::create(["descripcion"=>"Población en general"]);
-        Poblacion::create(["descripcion"=>"Productor(a)"]);
-        Poblacion::create(["descripcion"=>"Servidor(a) pública"]);
+        Poblacion::create(["descripcion"=>"Productores(as)"]);
+        Poblacion::create(["descripcion"=>"Servidores(as) públicos(as)"]);
     }
 }
