11febee518d6b7c9d1a1f9819d4a0e1ca9b3cfcf
Federico Sanchez <garfias.federico@gmail.com>
2025-04-04 13:17:04 -0600

    Adecuacion de reporte de PPA para el administrador

--- app/Exports/ItarExport.php
+++ app/Exports/ItarExport.php
@@ -15,12 +15,14 @@ class ItarExport implements FromCollection, WithHeadings
     public function collection()
     {
         //
-        return InformeAccion::select("id","nombre","descripcion", "objetivo","ejePEDClave","ejePEDDescripcion","temaPEDClave","temaPEDDescripcion","dependencia.dependenciaSiglas","informe_acciones.p_entrega",DB::raw("count(ia_bs.idBS) as bienes_servicios"))
+        return InformeAccion::select("id","nombre","descripcion", "objetivo","ejePEDClave","ejePEDDescripcion","temaPEDClave","temaPEDDescripcion",DB::raw("CONCAT(sectores.claveSector,' ',sectores.sector) as sector"),"dependencia.dependenciaSiglas","informe_acciones.p_entrega",DB::raw("count(ia_bs.idBS) as bienes_servicios"))
                                 ->join("dependencia","dependencia.idDependencia","=","informe_acciones.idDependencia")->orderBy("id")
                                 ->leftjoin("ia_bs","ia_bs.ia_id","=","informe_acciones.id")
                                 ->leftjoin("temaped","temaped.idTemaPED","=","informe_acciones.idTemaPED")
                                 ->leftjoin("ejeped","ejeped.idEjePED","=","temaped.idEjePED")
-                                ->groupBy("informe_acciones.id","informe_acciones.nombre","informe_acciones.descripcion","informe_acciones.objetivo","dependencia.dependenciaSiglas","informe_acciones.p_entrega","ejePEDClave","ejePEDDescripcion","temaPEDClave","temaPEDDescripcion")
+                                ->leftJoin("ia_alineacion","ia_alineacion.ia_id","=","informe_acciones.id")
+                                ->leftJoin("sectores","sectores.idSector","=","ia_alineacion.idSector")
+                                ->groupBy("informe_acciones.id","informe_acciones.nombre","informe_acciones.descripcion","informe_acciones.objetivo","dependencia.dependenciaSiglas","informe_acciones.p_entrega","ejePEDClave","ejePEDDescripcion","temaPEDClave","temaPEDDescripcion","sectores.claveSector","sectores.sector")
                                 ->get();
     }
 
