d737bf8a9ab19483187dab0a983abbab780cff92
Federico Sanchez <garfias.federico@gmail.com>
2025-04-07 09:27:49 -0600

    Actualización del Eslogan del 2025

--- resources/views/layouts/general.blade.php
+++ resources/views/layouts/general.blade.php
@@ -44,7 +44,7 @@
             <div id="content">
                 <div style="background-color:black;color:white;text-align:center; height:50px;display:table;width:100%">
                     <div style="display:table-cell; vertical-align:middle">
-                        "2024, BICENTENARIO DE LA INTEGRACIÓN DE OAXACA A LA REPÚBLICA MEXICANA"
+                        “2025, Bicentenario de la Primera Constitución Política del Estado Libre y Soberano de Oaxaca”
                     </div>
                 </div>
                 <div
