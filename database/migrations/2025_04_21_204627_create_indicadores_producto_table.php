86bb76213035a79362e634c7101b81857bb4be32
EdwinOrtiz <edwin.oc31@gmail.com>
2025-04-22 16:24:49 -0600

    Agregar la migración para crear la tabla indicadores_producto

---
+++ database/migrations/2025_04_21_204627_create_indicadores_producto_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('indicadores_producto', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('idProducto');
+            $table->string('tipo')->nullable();
+            $table->string('metodo_calculo')->nullable();
+            $table->string('frecuencia_medicion')->nullable();
+            $table->string('sentido_esperado')->nullable();
+            $table->string('unidad_medida_producto')->nullable();
+            $table->string('unidad_medida_indicador')->nullable();
+            $table->string('medio_verificacion_indicador')->nullable();
+        
+            $table->foreign('idProducto')->references('idProducto')->on('productos_pes')->onDelete('cascade');
+        });
+        
+        
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('indicadores_producto');
+    }
+};
