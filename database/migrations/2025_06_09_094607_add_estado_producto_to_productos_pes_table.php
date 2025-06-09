b437f034302f49a31e3ac46affc7368088396171
EdwinOrtiz <edwin.oc31@gmail.com>
2025-06-09 12:53:20 -0600

    Migración en  la tabla productosector

--- database/migrations/2025_06_09_094607_add_estado_producto_to_productos_pes_table.php
+++ database/migrations/2025_06_09_094607_add_estado_producto_to_productos_pes_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -13,8 +12,15 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('productos_pes', function (Blueprint $table) {
-            $table->string('estado_producto')->default('activo')->after('idDependencia');
+        Schema::table('productosector', function (Blueprint $table) {
+            $table->string('estado_producto')->default('activo')->after('idEstrategia');
+            // Agregar columna idDependencia con clave foránea
+            $table->integer('idDependencia')->nullable()->after('estado_producto');
+
+            $table->foreign('idDependencia')
+                ->references('idDependencia')
+                ->on('dependencia')
+                ->onDelete('cascade');
         });
     }
 
@@ -25,7 +31,9 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::table('productos_pes', function (Blueprint $table) {
+        Schema::table('productosector', function (Blueprint $table) {
+            $table->dropForeign(['idDependencia']);
+            $table->dropColumn('idDependencia');
             $table->dropColumn('estado_producto');
         });
     }
