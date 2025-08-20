dba78bafa8c1a136e47a5399900f9bc5966efc5d
EdwinOrtiz <edwin.oc31@gmail.com>
2025-08-20 14:51:51 -0600

    Aumento de cantidad de párrafos para introducción y conclusión en Informe

--- resources/views/informe/acciones.blade.php
+++ resources/views/informe/acciones.blade.php
@@ -729,8 +729,8 @@
         const contenedor = seccion === 'introduccion' ? '#contenedorIntroduccion' : '#contenedorConclusion';
         const numActuales = $(`${contenedor} .card`).length;
 
-        if (numActuales >= 2) {
-            Swal.fire('Límite alcanzado', 'Solo puedes agregar hasta 2 párrafos por sección.', 'info');
+        if (numActuales >= 4) {
+            Swal.fire('Límite alcanzado', 'Solo puedes agregar hasta 4 párrafos por sección.', 'info');
             return;
         }
 
