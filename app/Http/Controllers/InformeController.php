3a45bcf523183347775544edf02a59f4d6c5be82
Federico Sanchez <garfias.federico@gmail.com>
2025-09-23 15:31:59 -0600

    Incrementamos a 600 los caracteres de la introducción y conclusión

--- app/Http/Controllers/InformeController.php
+++ app/Http/Controllers/InformeController.php
@@ -1349,11 +1349,11 @@ Todos los textos deben estar dentro de una sección
      public function guardarInformeCoordinador(Request $request)
     {
         $request->validate([
-            'introduccion.*.parrafo' => 'nullable|string|max:400',
-            'conclusion.*.parrafo' => 'nullable|string|max:400',
+            'introduccion.*.parrafo' => 'nullable|string|max:600',
+            'conclusion.*.parrafo' => 'nullable|string|max:600',
         ], [
-            'introduccion.*.parrafo.max' => 'Cada párrafo de introducción debe tener como máximo 355 caracteres.',
-            'conclusion.*.parrafo.max' => 'Cada párrafo de conclusión debe tener como máximo 355 caracteres.',
+            'introduccion.*.parrafo.max' => 'Cada párrafo de introducción debe tener como máximo 600 caracteres.',
+            'conclusion.*.parrafo.max' => 'Cada párrafo de conclusión debe tener como máximo 600 caracteres.',
         ]);
 
 
