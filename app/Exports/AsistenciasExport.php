ce15d7a78cfd39d12374d4d622e46772e3542fab
Federico Sanchez <garfias.federico@gmail.com>
2026-03-17 22:58:29 -0600

    Adecuacion de reporte de asistencias, solo jalará 2026

--- app/Exports/AsistenciasExport.php
+++ app/Exports/AsistenciasExport.php
@@ -14,15 +14,16 @@ class AsistenciasExport implements FromCollection, WithHeadings
     */
     public function collection()
     {
-       return  Asistencias::select("tipo_enlace","nombre","dependenciaNombre","dependenciaSiglas","cargo","perfil","email","telefono",DB::raw("DATE_FORMAT(created_at,'%Y-%m-%d %H:%i:%s')"),"evento")
+       return  Asistencias::select("tipo_enlace","nombre","dependenciaNombre","dependenciaSiglas","cargo","nue","perfil","email","telefono",DB::raw("DATE_FORMAT(created_at,'%Y-%m-%d %H:%i:%s')"),"evento")
                     ->join("dependencia","dependencia.idDependencia","=","asistencias.dependenciasId")
                     ->where("evento","itar")
+                    ->whereYear("created_at",2026)
                     ->get();
     }
 
     public function headings():array{
 
         //return array_keys($this->collection()->first()->toArray());
-        return ["tipo enlace","nombre","dependencia","siglas","cargo","perfil","email","telefono","registro","evento"];
+        return ["tipo enlace","nombre","dependencia","siglas","cargo","nue","perfil","email","telefono","registro","evento"];
     }
 }
