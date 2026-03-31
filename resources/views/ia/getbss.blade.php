d991126b1ec86e44de5e7afcf2e627527a840868
Federico Sanchez <garfias.federico@gmail.com>
2026-03-30 22:17:44 -0600

    Se restringe la eliminacion de bienes o servicios hasta el id 1334 en

--- resources/views/ia/getbss.blade.php
+++ resources/views/ia/getbss.blade.php
@@ -9,8 +9,8 @@
             <th class="enc1" style="border: solid 1px gray;width:15%;text-align:center">Opciones</th>
         </tr>
     </thead>
-    <tbody id="body-indicadores" style="color:gray">                            
-        @if ($bss->count() > 0)                                            
+    <tbody id="body-indicadores" style="color:gray">
+        @if ($bss->count() > 0)
             @foreach ( $bss as $bs )
                 <tr>
                     <td style="border: solid 1px gray;width:10%;text-align:center">{{$bs->idBS}}</td>
@@ -18,7 +18,7 @@
                     <td style="border: solid 1px gray;">{{$bs->descripcionBS}}</td>
                     <td style="border: solid 1px gray;text-align:center">{{$bs->p_entrega}}</td>
                     <td style="border: solid 1px gray;text-align:center">
-                        <button 
+                        <button
                             class="btn btn-info btn-sm"
                             onclick="configurarDesglose({{ $bs->idBS }})">
                              Configurar Desglose
@@ -26,16 +26,18 @@
                     </td>
 
                     <td style="border: solid 1px gray;width:15%;text-align:center">
-                        <button class="btn btn-primary" onclick="editbs({{$bs->idBS}})"><i class="fas fa-edit"></i></button>                
-                        <button class="btn btn-danger" onclick="removebs({{$bs->idBS}})"><i class="fas fa-trash"></i></button>                
+                        <button class="btn btn-primary" onclick="editbs({{$bs->idBS}})"><i class="fas fa-edit"></i></button>
+                        @if($bs->idBS>1334)
+                            <button class="btn btn-danger" onclick="removebs({{$bs->idBS}})"><i class="fas fa-trash"></i></button>
+                        @endif
                     </td>
-                </tr>        
-            @endforeach        
+                </tr>
+            @endforeach
         @else
                 <tr id="emptybs" style="">
                     <td colspan="5" style="text-align: center;border:solid 1px gray;">No se han registrado bienes o servicios para este PPA!</td>
                 </tr>
         @endif
-        
-    </tbody>                                    
+
+    </tbody>
 </table>
