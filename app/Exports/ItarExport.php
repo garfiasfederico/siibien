1b6d34f03c9b0c168ba760c2d5b28ff88703e227
Federico Sanchez <garfias.federico@gmail.com>
2026-04-10 13:57:00 -0600

    Agregamos columna vigente al reporte excel de los PPA

--- app/Exports/ItarExport.php
+++ app/Exports/ItarExport.php
@@ -15,7 +15,7 @@ class ItarExport implements FromCollection, WithHeadings
     public function collection()
     {
         //
-        return InformeAccion::select("id","nombre","descripcion", "objetivo",DB::raw("if(informe_acciones.prioritario=0,'no','si') as prioritario"),"ejePEDClave","ejePEDDescripcion","temaPEDClave","temaPEDDescripcion",DB::raw("CONCAT(sectores.claveSector,' ',sectores.sector) as sector"),"dependencia.dependenciaSiglas","informe_acciones.p_entrega",DB::raw("count(ia_bs.idBS) as bienes_servicios"),
+        return InformeAccion::select("id","nombre","descripcion", "objetivo",DB::raw("if(informe_acciones.prioritario=0,'no','si') as prioritario"),DB::raw("if(informe_acciones.vigente=0,'no','si') as vigente"),"ejePEDClave","ejePEDDescripcion","temaPEDClave","temaPEDDescripcion",DB::raw("CONCAT(sectores.claveSector,' ',sectores.sector) as sector"),"dependencia.dependenciaSiglas","informe_acciones.p_entrega",DB::raw("count(ia_bs.idBS) as bienes_servicios"),
                                 DB::raw("(select GROUP_CONCAT(ia_bs.idBS SEPARATOR '|') from ia_bs where ia_bs.ia_id = informe_acciones.id) as BSids"))
                                 ->join("dependencia","dependencia.idDependencia","=","informe_acciones.idDependencia")->orderBy("id")
                                 ->leftjoin("ia_bs","ia_bs.ia_id","=","informe_acciones.id")
@@ -23,7 +23,7 @@ class ItarExport implements FromCollection, WithHeadings
                                 ->leftjoin("ejeped","ejeped.idEjePED","=","temaped.idEjePED")
                                 ->leftJoin("ia_alineacion","ia_alineacion.ia_id","=","informe_acciones.id")
                                 ->leftJoin("sectores","sectores.idSector","=","ia_alineacion.idSector")
-                                ->groupBy("informe_acciones.id","informe_acciones.nombre","informe_acciones.descripcion","informe_acciones.objetivo","dependencia.dependenciaSiglas","informe_acciones.p_entrega","ejePEDClave","ejePEDDescripcion","temaPEDClave","temaPEDDescripcion","sectores.claveSector","sectores.sector","prioritario")
+                                ->groupBy("informe_acciones.id","informe_acciones.nombre","informe_acciones.descripcion","informe_acciones.objetivo","dependencia.dependenciaSiglas","informe_acciones.p_entrega","ejePEDClave","ejePEDDescripcion","temaPEDClave","temaPEDDescripcion","sectores.claveSector","sectores.sector","prioritario","vigente")
                                 ->get();
     }
 
