d0ed20adc818fb051f3756e8f0f32a7ddd32a5e4
Federico Sanchez <garfias.federico@gmail.com>
2026-04-30 13:53:17 -0600

    Agregamos indicador 56 para modificacion 2024

--- resources/views/indicador/monitoreo.blade.php
+++ resources/views/indicador/monitoreo.blade.php
@@ -1098,7 +1098,7 @@
                                     '<i class="fas fa-arrow-up"></i></button>';
                             }
 
-                            if (anio == 2024 && (response.programados[x].idIndicador == 2 ||  response.programados[x].idIndicador == 30 ||  response.programados[x].idIndicador == 31 ||  response.programados[x].idIndicador == 53 ||  response.programados[x].idIndicador == 54 ||  response.programados[x].idIndicador == 55 ||  response.programados[x].idIndicador == 42 || response.programados[x].idIndicador == 102 || response.programados[x].idIndicador == 49 || response.programados[x].idIndicador == 50 )) {
+                            if (anio == 2024 && (response.programados[x].idIndicador == 2 ||  response.programados[x].idIndicador == 30 ||  response.programados[x].idIndicador == 31 ||  response.programados[x].idIndicador == 53 ||  response.programados[x].idIndicador == 54 ||  response.programados[x].idIndicador == 55 ||  response.programados[x].idIndicador == 42 || response.programados[x].idIndicador == 102 || response.programados[x].idIndicador == 49 || response.programados[x].idIndicador == 50 || response.programados[x].idIndicador == 56 )) {
                                 botonEditar =
                                     '<button class="btn btn-sm btn-success" title="Editar Valor" ' +
                                     'onclick="setDataValorProgramado(' + response.programados[x].idValoresIndicador + ')">' +
